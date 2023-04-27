96ee093fe7a28af12511a676996649a03a33c807
Akshay Mhatre <akshay.mhatre8989@gmail.com>
2023-04-27 13:26:37 +0200

    feat: dashboard app converted to remote app and adjusting shell-app with dashboard remote entry

--- apps/shell-app/module-federation.config.js
+++ apps/shell-app/module-federation.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   name: 'shell-app',
-  remotes: [],
+  remotes: ['dashboard'],
 };
--- apps/shell-app/src/app/app.routes.ts
+++ apps/shell-app/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'dashboard',
+    loadChildren: () => import('dashboard/Routes').then((m) => m.remoteRoutes),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
