abcf2fa5f4542f7f2ca81fe42d2725f99378d3c5
Akshay Mhatre <akshay.mhatre8989@gmail.com>
2023-04-28 11:20:28 +0200

    feat: updating remote url

--- projects/shell-app/webpack.config.js
+++ projects/shell-app/webpack.config.js
@@ -3,7 +3,7 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    "dashboard": "http://localhost:4201/remoteEntry.js",
+    "dashboard": "https://staging-angular-micro-frontend-with-mfe.vercel.app/remoteEntry.js",
   },
 
   shared: {
